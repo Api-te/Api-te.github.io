14daacce7458b92b17f5e0d43359156bdd78e3e5
Api-te <91590982+Api-te@users.noreply.github.com>
2021-11-01 09:37:59 +0800

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+$id_member = $_SESSION['id_member'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Beranda</title>
+</head>
+<body>
+<h1>Beranda Website</h1>
+<p>
+	Ini Merupakan Halaman Beranda Utama
+</p>
+</body>
+</html>
